40ef27e69d354c84cb17c4fb7cee997624d0664f
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2024-11-07 18:53:11 -0500

    auth route api set up

--- api/app.js
+++ api/app.js
@@ -1,12 +1,10 @@
 import express from 'express';
+import authRoute from './routes/auth.route.js'
 
 const app = express();
 
-app.use(express.json());
 
-app.get('/api', (req, res) => {
-    res.send({ message: "Hello from the Express server!" });
-});
+app.use('/api/auth', authRoute)
 
 app.listen(8800, () => {
     console.log('server is running')
---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const register = (req, res) => {
+    //db operations
+    console.log('register endpoint')
+    res.send('123')
+}
+
+export const login = (req, res) => {
+    //db operations
+}
+
+export const logout = (req, res) => {
+    //db operations
+} 
