e4ac189e9a90154bdf0072e0a75212946db185af
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2024-12-11 22:02:35 -0500

    -

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -9,6 +9,7 @@ function Login() {
   const [isLoading, setIsLoading] = useState(false);
   const {updateUser} = useContext(AuthContext)
 
+  
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -38,6 +39,7 @@ function Login() {
       setIsLoading(false);
     }
   };
+
   return (
     <div className="login">
       <div className="formContainer">
